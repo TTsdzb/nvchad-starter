4abb2085199e2703617b692273c91c62f6bfe921
TTsdzb <ttsdzb@outlook.com>
2024-10-17 09:43:02 +0800

    Initial modifications

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,24 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  -- Change the default values when calling conform.format()
+  -- This will also affect the default values for format_on_save/format_after_save
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+
+  -- Conform will run the formatter on save
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+  },
+
+  -- Conform will notify you when no formatters are available for the buffer
+  notify_no_formatters = true,
 }
 
 return options
