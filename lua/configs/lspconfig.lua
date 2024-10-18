fe1269611e8cb2ad30756f3f8a7c68e46797262e
TTsdzb <ttsdzb@outlook.com>
2024-10-18 13:20:22 +0800

    Add language server for js and ts

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,13 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pylsp", "rust_analyzer" }
+local servers = {
+  "html",
+  "cssls",
+  "vtsls",
+
+  "pylsp",
+  "rust_analyzer",
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
